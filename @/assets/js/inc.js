bb15340db4c37e7bbc3e7852b269a3b3f33550dd
Samuel John <opeoniye@gmai.com>
2022-05-03 15:41:17 +0100

    dclm developers community

---
+++ @/assets/js/inc.js
@@ -0,0 +1,2 @@
+// https://devdojo.com/tnylea/include-html-inside-of-html
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementsByTagName("include");for(var t=0;t<e.length;t++){let a=e[t];n(e[t].attributes.src.value,function(e){a.insertAdjacentHTML("afterend",e),a.remove()})}function n(e,t){fetch(e).then(e=>e.text()).then(e=>t(e))}});
